b2de726cd10f0ccdc27728b09ee72673cbf6aeed
ush189 <git@ush189.de>
2016-11-20 17:46:50 +0100

    layout styling

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,20 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
-import { Location }                 from '@angular/common';
+import { Component, OnInit }      from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
 
 import 'rxjs/add/operator/switchMap';
 
-import { Hero } from './hero';
+import { Hero }        from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
     moduleId: module.id,
     selector: 'my-hero-detail',
-    templateUrl: 'hero-detail.component.html'
+    templateUrl: 'hero-detail.component.html',
+    styleUrls: ['hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
-    @Input()
     hero: Hero;
 
     constructor(
