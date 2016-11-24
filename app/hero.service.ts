074db4c525b2e42612496788d95b5277433bf3d1
ush189 <git@ush189.de>
2016-11-24 20:27:53 +0100

    remove comment

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -24,7 +24,7 @@ export class HeroService {
     }
 
     private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
+        console.error('An error occurred', error);
         return Promise.reject(error.message || error);
     }
 }
