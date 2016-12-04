e6c203a1f655113a9636af3c66325e95e39b36de
ush189 <git@ush189.de>
2016-12-04 19:54:21 +0100

    implement hero search functionality

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,11 +7,14 @@ import { HttpModule }    from '@angular/http';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
+import './rxjs-extensions';
+
 import { AppComponent }        from './app.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent }  from './dashboard-component';
 import { HeroService }         from './hero.service';
+import { HeroSearchComponent}  from './hero-search.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -27,7 +30,8 @@ import { AppRoutingModule } from './app-routing.module';
         AppComponent,
         HeroesComponent,
         HeroDetailComponent,
-        DashboardComponent
+        DashboardComponent,
+        HeroSearchComponent
     ],
     bootstrap: [ AppComponent ],
     providers: [ HeroService ]
